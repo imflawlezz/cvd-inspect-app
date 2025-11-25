4b7f5ae595cf71ea84c8f4c21c6e605de2a7d640
imflawlezz <egorart6@gmail.com>
2025-11-25 20:32:44 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cvd-inspect',
+  webDir: 'dist'
+};
+
+export default config;
